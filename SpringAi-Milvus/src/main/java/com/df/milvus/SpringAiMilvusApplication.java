d10fad8799043189a41c534e6267af1ec43c94b2
DF <2285674575@qq.com>
2026-07-29 16:12:07 +0800

    SpringAi-Milvus创建

---
+++ SpringAi-Milvus/src/main/java/com/df/milvus/SpringAiMilvusApplication.java
@@ -0,0 +1,13 @@
+package com.df.milvus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAiMilvusApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringAiMilvusApplication.class, args);
+    }
+
+}
---
+++ SpringAi-Milvus/src/test/java/com/df/milvus/SpringAiMilvusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.df.milvus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAiMilvusApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
